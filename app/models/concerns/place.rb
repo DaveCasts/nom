3b39aa8f3cab8e815fd9115b9a05c8c15fcb4a9e
Dave Campos <dacamposc96@gmail.com>
2019-08-27 23:27:40 +0000

    Added validations and an error message to the 3 fields

--- app/models/concerns/place.rb
+++ app/models/concerns/place.rb
@@ -1,3 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: {minimum: 3}
+  validates :address, presence: true, length: {minimum: 3}
+  validates :description, presence: true, length: {minimum: 2}
 end
