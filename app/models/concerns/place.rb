5aec3ab834ac077b03bb43f17abcd43558c964ca
Dave Campos <dacamposc96@gmail.com>
2019-09-10 23:58:48 +0000

    got a photo uploaded and now an error

--- app/models/concerns/place.rb
+++ app/models/concerns/place.rb
@@ -2,10 +2,14 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :photos
+  has_many :pictures
+  has_many :captions
   geocoded_by :address
   after_validation :geocode
   
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true, length: {minimum: 3}
   validates :description, presence: true, length: {minimum: 2}
+  validates :picture, presence: true
+  validates :caption, presence: true, length: {minimum: 3}
 end
