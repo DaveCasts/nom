68d486897f97c9eb579ed45a3b7a018120dcbcb5
Dave Campos <dacamposc96@gmail.com>
2019-09-19 23:57:26 +0000

    Sending out emails when a comment is added to place

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
     @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params.merge(user: current_user))
+    
+    @place.comments.create(comment_params.merge(user_id: current_user.id))
     redirect_to place_path(@place)
   end
 
