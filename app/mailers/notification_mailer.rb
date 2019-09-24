39105162626321fd2a985b49ed57595fda0a9523
Dave Campos <dacamposc96@gmail.com>
2019-09-24 23:18:17 +0000

    Initial commit

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -9,4 +9,8 @@ class NotificationMailer < ApplicationMailer
       from: "no-reply@nomsterapp.com",
       subject: "A comment has been added to #{@place.name}")
   end
+
+  def forgotten_password
+    
+  end
 end
